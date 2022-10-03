96a773551198b43d6fc95a5fdd02007cd157b3c0
majidmoghadam <majid.moghadam@gmail.com>
2022-10-02 21:42:46 -0400

    Chapter 5 - Added Alien Spawning Logic

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -27,6 +27,61 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        currentSpawnTime += Time.deltaTime;     //accumulates time passed each frame
+
+        if (currentSpawnTime > generatedSpawnTime)
+        {
+            currentSpawnTime = 0;   //resets timer after a spawn occurs
+            //spawn-time randomizer
+            generatedSpawnTime = Random.Range(minSpawnTime, maxSpawnTime);
+            //preventative code to make sure there are some aliens but not more than total
+            if (aliensPerSpawn > 0 && aliensOnScreen < totalAliens)
+            {
+                //array keeps track of where we spawn aliens each wave
+                List<int> previousSpawnLocations = new List<int>();
+                //limits number of aliens to the number of spawn points
+                if (aliensPerSpawn > spawnPoints.Length)
+                {
+                    aliensPerSpawn = spawnPoints.Length - 1;
+                }
+                //more preventative code.if aliensperspawn exceeds the max, then reduce
+                //the number of spawns. This prevents spawning more than the max configured
+                aliensPerSpawn = (aliensPerSpawn > totalAliens) ? aliensPerSpawn - totalAliens : aliensPerSpawn;
+
+                //actual spawning code, loop iterates once for each spawned alien
+                for (int i = 0; i < aliensPerSpawn; i++)
+                {
+                    if (aliensOnScreen < maxAliensOnScreen)
+                    {
+                        aliensOnScreen += 1;
+                        // the random generated spawn point number
+                        //-1 means it hasn't been assigned a spawn point yet
+                        int spawnPoint = -1;
+                        // loop until it finds a spawn point number that hasn't
+                        //been used already
+                        while (spawnPoint == -1)
+                        {
+                            // produces a random possible spawn point
+                            int randomNumber = Random.Range(0, spawnPoints.Length - 1);
+                            // if random number doesn't match a previous spawnpoint
+                            // then that number is added to the SpanLocations array
+                            // and used as the SpawnPoint for that alien
+                            if (!previousSpawnLocations.Contains(randomNumber))
+                            {
+                                previousSpawnLocations.Add(randomNumber);
+                                spawnPoint = randomNumber;
+                            }
+                        }
+                        //grabs the spawn location using the spawnpoint generated
+                        //in the while loop
+                        GameObject spawnLocation = spawnPoints[spawnPoint];
+                        //create an instance of the alien using the prefab
+                        GameObject newAlien = Instantiate(alien) as GameObject;
+                        //this positions the alien at the spawnpoint
+                        newAlien.transform.position = spawnLocation.transform.position;
+                    }
+                }
+            }
+        }
     }
 }
