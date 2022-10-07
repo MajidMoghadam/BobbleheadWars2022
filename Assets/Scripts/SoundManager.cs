ed94e2a190c8ef853a2e25dd06be536cc1bb6d7c
majidmoghadam <majid.moghadam@gmail.com>
2022-10-07 10:46:48 -0400

    Chapter 7 - Set Up SoundManager

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    //static reference to SoundManager easily accessible from anywhere
+    //in our scripts
+    public static SoundManager Instance = null;
+
+    //reference to 2nd Audio Source added for special effects
+    private AudioSource soundEffectAudio;
+
+    //all sound effects requied in game
+    public AudioClip gunFire;
+    public AudioClip upgradedGunFire;
+    public AudioClip hurt;
+    public AudioClip alienDeath;
+    public AudioClip marineDeath;
+    public AudioClip victory;
+    public AudioClip elevatorArrived;
+    public AudioClip powerUpPickup;
+    public AudioClip powerUpAppear;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        AudioSource[] sources = GetComponents<AudioSource>();
+        foreach (AudioSource source in sources)
+        {
+            if (source.clip == null)
+            {
+                soundEffectAudio = source;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
